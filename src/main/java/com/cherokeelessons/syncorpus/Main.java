cf57c80616d5082a80f82926bbfb79563289be2f
mjoyner <mjoyner@vbservices.net>
2019-10-27 11:10:14 -0400

    placeholder main

---
+++ src/main/java/com/cherokeelessons/syncorpus/Main.java
@@ -0,0 +1,15 @@
+package com.cherokeelessons.syncorpus;
+
+/**
+ * 
+ * @author muksihs
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
